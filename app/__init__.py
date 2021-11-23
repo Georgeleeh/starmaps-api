a649539c2600390c5e7d7347edf351c63823fc11
Georgeleeh <me+code@georgeleeh.com>
2021-11-23 11:23:19 +0000

    add format_datetime filter

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_cors import CORS
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -11,3 +12,16 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 
 from app import routes
+
+@app.template_filter()
+def format_datetime(value, format='full'):
+    if value is None:
+        return None
+        
+    if format == 'full':
+        format="%Y-%m-%d %H:%M"
+    elif format == 'date':
+        format="%Y-%m-%d"
+    elif format == 'time':
+        format="%H:%M"
+    return datetime.fromtimestamp(value).strftime(format)
